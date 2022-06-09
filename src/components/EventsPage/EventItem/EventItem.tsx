732419b365dc5dc4dfc4ddab86de114256c36fd6
rasul-surname <rasul.surname@mail.ru>
2022-06-09 10:14:37 +0300

    task-4 ограничил отрывок 6 словами на странице Календарь

--- src/components/EventsPage/EventItem/EventItem.tsx
+++ src/components/EventsPage/EventItem/EventItem.tsx
@@ -11,12 +11,16 @@ interface InterfaceEventItem {
 
 const EventItem: React.FC<InterfaceEventItem> = ({id, image, title, description}) => {
 
+    function limitStr(str: string, wordCount: number) {
+        return str.split(' ').slice(0, wordCount).join(' ') + '...';
+    }
+
     return (
         <div className={classes.card} key={id}>
             <img className={classes.card__img} src={image} alt=""/>
             <div>
-                <p>{title}</p>
-                <p>{description}</p>
+                <p className={classes.card__title}>{title}</p>
+                <p className={classes.card__desc}>{limitStr(description, 5)}</p>
             </div>
         </div>
     )
