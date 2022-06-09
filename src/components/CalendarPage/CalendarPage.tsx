3c0c1df07eb9aa792ca3fec59c86e73566b61545
rasul-surname <rasul.surname@mail.ru>
2022-06-09 20:38:26 +0300

    task-8 вывел список событий - страница События

--- src/components/CalendarPage/CalendarPage.tsx
+++ src/components/CalendarPage/CalendarPage.tsx
@@ -1,9 +1,27 @@
+import React, {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {useTypedSelector} from "../../store/hooks/useTypedSelector";
+import {fetchEvents} from "../../store/action-creators/events";
+import CalendarItem from "./CalendarItem/CalendarItem";
+import classes from './CalendarPage.module.css';
+
 const CalendarPage = () => {
+    const {listEvents} = useTypedSelector(state => state.events);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // @ts-ignore
+        if (listEvents.length === 0) dispatch(fetchEvents());
+    }, []);
 
     return (
-        <>
-            CalendarPage
-        </>
+        <div className={classes.cards}>
+            {listEvents.map((event) => {
+                return (
+                    <CalendarItem id={event.id} title={event.title} image={event.image} date={event.date} />
+                )
+            })}
+        </div>
     )
 }
 
